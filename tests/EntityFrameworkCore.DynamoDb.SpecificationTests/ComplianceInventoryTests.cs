c2f9a18d14a395d9cdaa7eeca99eb7892a09e97a
Jonas Ha <jonas-ha@outlook.com>
2026-05-08 13:51:46 -0400

    Add specification compliance inventory

---
+++ tests/EntityFrameworkCore.DynamoDb.SpecificationTests/ComplianceInventoryTests.cs
@@ -0,0 +1,31 @@
+namespace EntityFrameworkCore.DynamoDb.SpecificationTests;
+
+public sealed class ComplianceInventoryTests
+{
+    [Fact(Timeout = TestConfiguration.DefaultTimeout)]
+    public void Compliance_inventory_tracks_current_spec_areas()
+    {
+        var inventoryPath = Path.Combine(AppContext.BaseDirectory, "ComplianceInventory.md");
+        File.Exists(inventoryPath).Should().BeTrue();
+
+        var inventory = File.ReadAllText(inventoryPath);
+        string[] expectedAreas =
+        [
+            "Northwind Where predicates",
+            "Northwind Select projections",
+            "Northwind string functions",
+            "Northwind ordering/paging",
+            "Scalar/single-result operators",
+            "Aggregates",
+            "Joins/includes/navigation queries",
+            "Query filters",
+            "Null semantics",
+            "SaveChanges",
+            "Type mapping/value conversion",
+            "Complex types/primitive collections"
+        ];
+
+        foreach (var area in expectedAreas)
+            inventory.Should().Contain(area);
+    }
+}
