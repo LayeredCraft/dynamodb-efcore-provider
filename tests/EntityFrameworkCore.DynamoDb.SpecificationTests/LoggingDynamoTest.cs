491196b6009f59d4a40f7a94339e9cf4d07fc30e
Nick Cipollina <ncipollina@gmail.com>
2026-05-20 13:10:51 -0400

    test(tests): assert actual context initialization logs

--- tests/EntityFrameworkCore.DynamoDb.SpecificationTests/LoggingDynamoTest.cs
+++ tests/EntityFrameworkCore.DynamoDb.SpecificationTests/LoggingDynamoTest.cs
@@ -1,10 +1,11 @@
-using EntityFrameworkCore.DynamoDb.SpecificationTests.TestUtilities;
 using System.Reflection;
+using EntityFrameworkCore.DynamoDb.Extensions;
 using EntityFrameworkCore.DynamoDb.Infrastructure;
+using EntityFrameworkCore.DynamoDb.Infrastructure.Internal;
+using EntityFrameworkCore.DynamoDb.SpecificationTests.TestUtilities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Diagnostics;
 using Microsoft.EntityFrameworkCore.Diagnostics.Internal;
-using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.TestUtilities;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -15,39 +16,30 @@ public sealed class LoggingDynamoTest : LoggingTestBase
     [ConditionalFact(Skip = SkipReason.PartitionKeyRequiredOnAllEntities)]
     public override void InvalidIncludePathError_throws_by_default() { }
 
-    protected override string ActualMessage(Func<IServiceCollection, DbContextOptionsBuilder> optionsActions)
-    {
-        var options = optionsActions(new ServiceCollection()).Options;
-        var coreOptions = options.FindExtension<CoreOptionsExtension>();
-
-        var optionsFragment = DefaultOptions;
-        if (coreOptions?.QueryTrackingBehavior == QueryTrackingBehavior.NoTracking)
-        {
-            optionsFragment = "NoTracking " + optionsFragment;
-        }
-
-        if (coreOptions?.IsSensitiveDataLoggingEnabled == true)
-        {
-            optionsFragment = "SensitiveDataLoggingEnabled " + optionsFragment;
-        }
-
-        return ExpectedMessage(optionsFragment);
-    }
-
     protected override TestLogger CreateTestLogger()
         => new TestLogger<TestLoggingDefinitions>();
 
     protected override DbContextOptionsBuilder CreateOptionsBuilder(IServiceCollection services)
     {
-        var serviceProvider = DynamoTestStoreFactory.Instance
-            .AddProviderServices(services)
+        var serviceProvider = services
+            .AddEntityFrameworkDynamo()
             .BuildServiceProvider(validateScopes: true);
 
         return new DbContextOptionsBuilder()
             .UseDynamo()
             .UseInternalServiceProvider(serviceProvider)
-            .ConfigureWarnings(w => w.Default(WarningBehavior.Throw));
+            .ConfigureWarnings(w => w
+                .Default(WarningBehavior.Throw)
+                .Log(CoreEventId.SensitiveDataLoggingEnabledWarning));
     }
+
+    protected override string DefaultOptions
+        => new DbContextOptionsBuilder()
+            .UseDynamo()
+            .Options
+            .FindExtension<DynamoDbOptionsExtension>()!
+            .Info
+            .LogFragment;
 
     protected override string ProviderName => "EntityFrameworkCore.DynamoDb";
 
