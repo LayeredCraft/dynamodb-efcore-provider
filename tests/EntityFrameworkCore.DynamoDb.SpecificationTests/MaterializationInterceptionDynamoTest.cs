fdd25a942bdeec5456c7403fbaec66209b3953a8
Jonas Ha <jonas-ha@outlook.com>
2026-05-21 20:07:22 -0400

    test(materialization): test change to trigger format.

--- tests/EntityFrameworkCore.DynamoDb.SpecificationTests/MaterializationInterceptionDynamoTest.cs
+++ tests/EntityFrameworkCore.DynamoDb.SpecificationTests/MaterializationInterceptionDynamoTest.cs
@@ -10,49 +10,86 @@ namespace EntityFrameworkCore.DynamoDb.SpecificationTests;
 
 [Collection(DynamoSpecificationCollection.Name)]
 public sealed class MaterializationInterceptionDynamoTest(NonSharedFixture fixture)
-    : MaterializationInterceptionTestBase<MaterializationInterceptionDynamoTest.DynamoLibraryContext>(fixture)
+    : MaterializationInterceptionTestBase<
+        MaterializationInterceptionDynamoTest.DynamoLibraryContext>(fixture)
 {
     [ConditionalFact]
     public void Check_all_tests_overridden()
-        => DynamoTestHelpers.AssertAllTestMethodsOverridden(typeof(MaterializationInterceptionDynamoTest));
+    {
+        DynamoTestHelpers.AssertAllTestMethodsOverridden(
+            typeof(MaterializationInterceptionDynamoTest));
+    }
 
     public override Task Binding_interceptors_are_used_by_queries(bool inject, bool usePooling)
-        => base.Binding_interceptors_are_used_by_queries(inject, usePooling);
+    {
+        return base.Binding_interceptors_are_used_by_queries(inject, usePooling);
+    }
 
-    public override Task Binding_interceptors_are_used_when_creating_instances(bool inject, bool usePooling)
-        => base.Binding_interceptors_are_used_when_creating_instances(inject, usePooling);
+    public override Task Binding_interceptors_are_used_when_creating_instances(
+        bool inject,
+        bool usePooling)
+    {
+        return base.Binding_interceptors_are_used_when_creating_instances(inject, usePooling);
+    }
 
-    public override Task Intercept_query_materialization_for_empty_constructor(bool inject, bool usePooling)
-        => base.Intercept_query_materialization_for_empty_constructor(inject, usePooling);
+    public override Task Intercept_query_materialization_for_empty_constructor(
+        bool inject,
+        bool usePooling)
+    {
+        return base.Intercept_query_materialization_for_empty_constructor(inject, usePooling);
+    }
 
     [ConditionalTheory(Skip = SkipReason.OwnedEntityTypesNotSupported)]
-    public override Task Intercept_query_materialization_with_owned_types(bool async, bool usePooling)
-        => Task.CompletedTask;
+    public override Task Intercept_query_materialization_with_owned_types(
+        bool async,
+        bool usePooling)
+    {
+        return Task.CompletedTask;
+    }
 
     [ConditionalTheory(Skip = SkipReason.OwnedEntityTypesNotSupported)]
-    public override Task Intercept_query_materialization_with_owned_types_projecting_collection(bool async, bool usePooling)
-        => Task.CompletedTask;
+    public override Task Intercept_query_materialization_with_owned_types_projecting_collection(
+        bool async,
+        bool usePooling)
+    {
+        return Task.CompletedTask;
+    }
 
-    public override Task Intercept_query_materialization_for_full_constructor(bool inject, bool usePooling)
-        => base.Intercept_query_materialization_for_full_constructor(inject, usePooling);
+    public override Task Intercept_query_materialization_for_full_constructor(
+        bool inject,
+        bool usePooling)
+    {
+        return base.Intercept_query_materialization_for_full_constructor(inject, usePooling);
+    }
 
-    public override Task Multiple_materialization_interceptors_can_be_used(bool inject, bool usePooling)
-        => base.Multiple_materialization_interceptors_can_be_used(inject, usePooling);
+    public override Task Multiple_materialization_interceptors_can_be_used(
+        bool inject,
+        bool usePooling)
+    {
+        return base.Multiple_materialization_interceptors_can_be_used(inject, usePooling);
+    }
 
-    protected override ITestStoreFactory TestStoreFactory => DynamoTestStoreFactory.Instance;
+    protected override ITestStoreFactory TestStoreFactory
+    {
+        get { return DynamoTestStoreFactory.Instance; }
+    }
 
     protected override IServiceCollection InjectInterceptors(
         IServiceCollection serviceCollection,
         IEnumerable<ISingletonInterceptor> injectedInterceptors)
-        => base.InjectInterceptors(
+    {
+        return base.InjectInterceptors(
             serviceCollection.AddEntityFrameworkDynamo(),
             injectedInterceptors);
+    }
 
     protected override DbContextOptionsBuilder AddOptions(DbContextOptionsBuilder builder)
-        => base
+    {
+        return base
             .AddOptions(builder)
             .UseDynamo(options => options.DynamoDbClient(DynamoTestStoreFactory.Instance.Client))
             .ConfigureWarnings(w => w.Ignore(DynamoEventId.ScanLikeQueryDetected));
+    }
 
     public class DynamoLibraryContext(DbContextOptions options) : LibraryContext(options)
     {
