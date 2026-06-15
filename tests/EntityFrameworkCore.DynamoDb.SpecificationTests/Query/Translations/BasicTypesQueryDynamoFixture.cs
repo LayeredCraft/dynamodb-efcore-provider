ec7af8a63bcd67b7f830c7b4103245700d38d808
Jonas Ha <jonas-ha@outlook.com>
2026-06-15 15:34:03 -0400

    test: add comparison translation spec coverage

---
+++ tests/EntityFrameworkCore.DynamoDb.SpecificationTests/Query/Translations/BasicTypesQueryDynamoFixture.cs
@@ -0,0 +1,49 @@
+using EntityFrameworkCore.DynamoDb.Diagnostics;
+using EntityFrameworkCore.DynamoDb.Infrastructure;
+using EntityFrameworkCore.DynamoDb.SpecificationTests.TestUtilities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.EntityFrameworkCore.Query.Translations;
+using Microsoft.EntityFrameworkCore.TestModels.BasicTypesModel;
+using Microsoft.EntityFrameworkCore.TestUtilities;
+
+namespace EntityFrameworkCore.DynamoDb.SpecificationTests.Query.Translations;
+
+/// <summary>Basic types query fixture for DynamoDB translation specification tests.</summary>
+public class BasicTypesQueryDynamoFixture : BasicTypesQueryFixtureBase, IDynamoSpecificationFixture
+{
+    public TestSqlLoggerFactory TestSqlLoggerFactory => (TestSqlLoggerFactory)ListLoggerFactory;
+
+    protected override ITestStoreFactory TestStoreFactory => DynamoTestStoreFactory.Instance;
+
+    protected override bool UsePooling => false;
+
+    protected override bool ShouldLogCategory(string logCategory)
+        => DynamoSpecificationFixtureExtensions.ShouldLogDynamoSql(logCategory);
+
+    public override DbContextOptionsBuilder AddOptions(DbContextOptionsBuilder builder)
+        => base
+            .AddOptions(builder)
+            .ConfigureWarnings(warnings
+                => warnings
+                    .Ignore(CoreEventId.ManyServiceProvidersCreatedWarning)
+                    .Ignore(DynamoEventId.ScanLikeQueryDetected))
+            .UseDynamo(options
+                => options
+                    .DynamoDbClient(DynamoTestStoreFactory.Instance.Client)
+                    .TransactionOverflowBehavior(TransactionOverflowBehavior.UseChunking));
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder, DbContext context)
+    {
+        base.OnModelCreating(modelBuilder, context);
+
+        modelBuilder
+            .Entity<BasicTypesEntity>()
+            .ToTable("BasicTypesEntities")
+            .HasPartitionKey(e => e.Id);
+        modelBuilder
+            .Entity<NullableBasicTypesEntity>()
+            .ToTable("NullableBasicTypesEntities")
+            .HasPartitionKey(e => e.Id);
+    }
+}
