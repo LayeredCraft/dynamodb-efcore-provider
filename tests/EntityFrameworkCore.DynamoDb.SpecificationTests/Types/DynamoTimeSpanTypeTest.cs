2ca1f7579dade98fccfa650809d3ff231a862f53
Jonas Ha <jonas-ha@outlook.com>
2026-07-04 08:47:26 -0400

    test(tests): add expression namespace to type specs

--- tests/EntityFrameworkCore.DynamoDb.SpecificationTests/Types/DynamoTimeSpanTypeTest.cs
+++ tests/EntityFrameworkCore.DynamoDb.SpecificationTests/Types/DynamoTimeSpanTypeTest.cs
@@ -1,3 +1,4 @@
+using System.Linq.Expressions;
 using EntityFrameworkCore.DynamoDb.Diagnostics;
 using EntityFrameworkCore.DynamoDb.SpecificationTests.TestUtilities;
 using Microsoft.EntityFrameworkCore;
